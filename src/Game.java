16b737a3f80282a512cb35d435d8523182d87308
Sozuww <sozuww@gmail.com>
2025-04-11 10:49:47 -0700

    Added a background and cookie

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Game {
+    private final Viewer window;
+    private long cookies;
+    private int clickVal;
+    private int upgradeMultiplier;
+
+    public Game()
+    {
+        this.window = new Viewer(this);
+        cookies = 0;
+        clickVal = 1;
+        upgradeMultiplier = 1;
+    }
+
+    public long getCookies() {
+        return cookies;
+    }
+
+    public int getClickVal() {
+        return clickVal;
+    }
+
+    public int getUpgradeMultiplier() {
+        return upgradeMultiplier;
+    }
+
+    public void incrementCookies()
+    {
+        cookies += clickVal * upgradeMultiplier;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+    }
 }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Viewer {
+public class Viewer extends JFrame {
+    private Game game;
+    private Image backgroundImage;
+    private Image cookieImage;
+
+
+    public Viewer(Game game)
+    {
+        this.game = game;
+        backgroundImage = new ImageIcon("Resources/backgroundImage.jpeg").getImage();
+        cookieImage = new ImageIcon("Resources/PerfectCookie.png").getImage();
+        this.setTitle("Cookie Clicker");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1200, 800);
+        this.setVisible(true);
+
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e)
+            {
+                int centerX = getWidth() / 2;
+                int centerY = getHeight() / 2;
+                int mid = 150;
+
+                // Checks if the click occurs within 200 pixels of the center of the screen
+                if (e.getX() > centerX - mid && e.getX() < centerX + mid &&
+                e.getY() > centerY - mid && e.getY() < centerY + mid)
+                {
+                    game.incrementCookies();
+                    System.out.println("cookies: " + game.getCookies());
+                }
+
+            }
+        });
+    }
+
+    public void paint(Graphics g)
+    {
+        g.drawImage(backgroundImage, 0, 0, 1200, 800, this);
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        g.drawImage(cookieImage, centerX - 150, centerY - 150, 300, 300, null);
+        g.setColor(Color.RED);
+        g.drawRect(centerX - 150,centerY - 150, 300, 300);
+
+
+    }
 }
